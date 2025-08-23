3ef6c73a5ae78a3fff761176f59dd0890e5e5d77
zhyan <ijoelyang@163.com>
2025-08-23 14:23:20 +0800

    Update build23.sh
    PACKAGES="$PACKAGES luci-i18n-openvpn-server-zh-cn"
    PACKAGES="$PACKAGES luci-i18n-openvpn-zh-cn"
    PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
    PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
    PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -54,9 +54,14 @@ PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-openvpn-server-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-openvpn-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"
+
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
